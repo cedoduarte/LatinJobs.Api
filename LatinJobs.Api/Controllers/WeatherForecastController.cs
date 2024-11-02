b798e1c41d44bc88c1a36c54420b6335045a1794
Carlos Duarte <carlosduarte.1@hotmail.com>
2024-11-02 16:21:24 -0700

    test: simple api

--- LatinJobs.Api/Controllers/WeatherForecastController.cs
+++ LatinJobs.Api/Controllers/WeatherForecastController.cs
@@ -19,15 +19,15 @@ namespace LatinJobs.Api.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<IActionResult> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var array = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            }).ToArray();
+            return Ok(array);
         }
     }
 }
